1f20948d3d1804763475675fe7642840159fb2b0
Anthony Castelli <mranthonycastelli@gmail.com>
2025-09-18 09:29:04 -0400

    Cleanup Webpack
    This cleans up the webpack file and removes more warnings

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,38 +1,39 @@
-const webpack = require('webpack')
-const path = require('path')
+const webpack = require("webpack");
+const path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebPackPlugin = require('copy-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebPackPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
-const publicPath = path.resolve(__dirname, 'public')
-const srcPath = path.resolve(__dirname, 'src')
-const buildPath = path.resolve(__dirname, 'build')
+const publicPath = path.resolve(__dirname, "public");
+const srcPath = path.resolve(__dirname, "src");
+const buildPath = path.resolve(__dirname, "build");
 
-const analyze = process.argv.includes('--analyze')
-const defaultLibTarget = 'umd'
-const libTarget = process.env.LIB_TARGET || defaultLibTarget
-const simulateBlocking = process.argv.includes('block')
-const webpackMode = process.argv.includes('--mode')
-  ? process.argv[process.argv.indexOf('--mode') + 1]
-  : 'production'
-const isDev = webpackMode === 'development'
+const analyze = process.argv.includes("--analyze");
+const defaultLibTarget = "umd";
+const libTarget = process.env.LIB_TARGET || defaultLibTarget;
+const simulateBlocking = process.argv.includes("block");
+const webpackMode = process.argv.includes("--mode")
+  ? process.argv[process.argv.indexOf("--mode") + 1]
+  : "production";
+const isDev = webpackMode === "development";
 
 const sharedPlugins = [
   new webpack.DefinePlugin({
-    'process.env': {
+    "process.env": {
       CDN_ENV: JSON.stringify(process.env.CDN_ENV),
       CDN_URL: JSON.stringify(process.env.CDN_URL),
-      DEPLOYMENT_TYPE: JSON.stringify(process.env.DEPLOYMENT_TYPE || 'local'),
+      DEPLOYMENT_TYPE: JSON.stringify(process.env.DEPLOYMENT_TYPE || "local"),
       PRISM_VERSION: JSON.stringify(process.env.npm_package_version),
       NODE_ENV: JSON.stringify(webpackMode),
       SIMULATE_BLOCKING_SCRIPT: simulateBlocking,
     },
   }),
-]
+];
 
 const staticAssetPlugins = [
   // copy manifest and images directory from public to build
@@ -40,26 +41,34 @@ const staticAssetPlugins = [
   new CopyWebPackPlugin({
     patterns: [
       //   { from: 'public/font', to: 'font' },
-      { from: 'public/images', to: 'images', globOptions: { ignore: ['**/.DS_Store'] } },
-      { from: 'public/translations', to: 'translations', globOptions: { ignore: ['**/.DS_Store'] } },
-      isDev && { from: 'public/manifest.json', to: '' },
+      {
+        from: "public/images",
+        to: "images",
+        globOptions: { ignore: ["**/.DS_Store"] },
+      },
+      {
+        from: "public/translations",
+        to: "translations",
+        globOptions: { ignore: ["**/.DS_Store"] },
+      },
+      isDev && { from: "public/manifest.json", to: "" },
     ].filter(Boolean),
   }),
-]
+];
 
 if (libTarget === defaultLibTarget) {
-  sharedPlugins.push(...staticAssetPlugins)
+  sharedPlugins.push(...staticAssetPlugins);
 }
 
 // show bundle analyzer gui when using build:stats or start:stats
 if (analyze) {
-  sharedPlugins.push(new BundleAnalyzerPlugin())
+  sharedPlugins.push(new BundleAnalyzerPlugin());
 }
 
 const devPlugins = [
   ...sharedPlugins,
   new HtmlWebpackPlugin({
-    template: path.resolve(publicPath, 'index.hbs'),
+    template: path.resolve(publicPath, "index.hbs"),
     simulateBlockingScript: simulateBlocking,
     minify: {
       removeComments: false,
@@ -76,30 +85,30 @@ const devPlugins = [
     inject: false,
   }),
   new ReactRefreshWebpackPlugin(),
-]
+];
 
 const prodPlugins = [
   ...sharedPlugins,
   // clean build folder when running `npm start build`
   new CleanWebpackPlugin(),
-]
+];
 
 // show sourcemaps when building with npm build:local for debugging
-const prodDevtool = process.env.SOURCEMAPS === 'true' ? 'source-map' : false
-const devtool = isDev ? 'eval-cheap-module-source-map' : prodDevtool
+const prodDevtool = process.env.SOURCEMAPS === "true" ? "source-map" : false;
+const devtool = isDev ? "eval-cheap-module-source-map" : prodDevtool;
 
 const resolve = {
   alias: {
-    '@components': path.resolve(__dirname, 'src/ui/components'),
-    '~': path.resolve(__dirname, 'src/'),
+    "@components": path.resolve(__dirname, "src/ui/components"),
+    "~": path.resolve(__dirname, "src/"),
   },
-  extensions: ['*', '.ts', '.tsx', '.js', '.jsx'],
-  modules: [srcPath, 'node_modules'],
-}
+  extensions: ["*", ".ts", ".tsx", ".js", ".jsx"],
+  modules: [srcPath, "node_modules"],
+};
 
 module.exports = {
   entry: {
-    prism: './src/index.tsx',
+    prism: "./src/index.tsx",
   },
   mode: webpackMode,
   devtool,
@@ -110,16 +119,16 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
               presets: [
-                '@babel/preset-env',
-                ['@babel/preset-react', { runtime: 'automatic' }],
-                '@babel/preset-typescript',
+                "@babel/preset-env",
+                ["@babel/preset-react", { runtime: "automatic" }],
+                "@babel/preset-typescript",
               ],
               plugins: [
-                '@babel/plugin-transform-runtime',
-                isDev && require.resolve('react-refresh/babel'),
+                "@babel/plugin-transform-runtime",
+                isDev && require.resolve("react-refresh/babel"),
               ].filter(Boolean),
             },
           },
@@ -127,51 +136,52 @@ module.exports = {
       },
       {
         test: /\.(svg|png)$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[path][name].[ext]',
+          name: "[path][name].[ext]",
         },
       },
       {
         test: /\.hbs$/,
-        loader: 'handlebars-loader',
+        loader: "handlebars-loader",
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
     ],
   },
-  experiments: process.env.DEPLOYMENT_TYPE === 'module' ? { outputModule: true } : {},
+  experiments:
+    process.env.DEPLOYMENT_TYPE === "module" ? { outputModule: true } : {},
   resolve,
   output: (() => {
-    if (process.env.DEPLOYMENT_TYPE === 'module') {
+    if (process.env.DEPLOYMENT_TYPE === "module") {
       return {
         clean: true,
-        filename: 'prism.js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: "prism.js",
+        path: path.resolve(__dirname, "dist"),
         library: {
-          type: 'module',
+          type: "module",
         },
         environment: {
           module: true,
         },
-        publicPath: 'auto',
-      }
+        publicPath: "auto",
+      };
     }
     // Default: CDN/UMD
     return {
       clean: true,
-      filename: isDev ? 'default/[name].js' : '[name].js',
+      filename: isDev ? "default/[name].js" : "[name].js",
       library: {
-        name: 'Prism',
+        name: "Prism",
         type: defaultLibTarget,
         umdNamedDefine: true,
       },
       path: buildPath,
-      publicPath: 'auto',
-      globalObject: 'this',
-    }
+      publicPath: "auto",
+      globalObject: "this",
+    };
   })(),
   devServer: {
     port: 3000,
@@ -179,9 +189,9 @@ module.exports = {
     compress: true,
     open: true,
     historyApiFallback: true,
-    allowedHosts: 'all',
+    allowedHosts: "all",
     server: {
-      type: 'https',
+      type: "https",
     },
   },
   // Suppress performance warnings for single-bundle CDN builds
@@ -192,9 +202,9 @@ module.exports = {
     maxAssetSize: 3 * 1024 * 1024,
     assetFilter: (filename) => !/\.map$/.test(filename),
   },
-  watchOptions: { ignored: ['/src/tailwind.css'] },
+  watchOptions: { ignored: ["/src/tailwind.css"] },
   optimization:
-    process.env.DEPLOYMENT_TYPE === 'module'
+    process.env.DEPLOYMENT_TYPE === "module"
       ? {
           minimize: false,
         }
@@ -211,11 +221,11 @@ module.exports = {
                   comments: false,
                 },
                 compress: {
-                  pure_funcs: ['console.info', 'console.debug', 'console.warn'],
+                  pure_funcs: ["console.info", "console.debug", "console.warn"],
                 },
               },
             }),
           ],
         },
   plugins: isDev ? devPlugins : prodPlugins,
-}
+};

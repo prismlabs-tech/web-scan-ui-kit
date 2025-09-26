018b37dfaacb9847102ff6ac0cfd6e3e150837c9
Christoph Leitner <christophl@nakedlabs.com>
2025-09-26 11:43:25 +0200

    fix regex in build script, add debug lines

--- build.sh
+++ build.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env bash
 set -e # exit on error
 
-# install dependencies
+echo "-------------------------------------------------------------------"
+echo "Run npm install ..."
+echo "-------------------------------------------------------------------"
 npm ci
 
 # check whether the commit is tagged so we can decide which environment to build
-STAGE_REGEXP="^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)-[rc](?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
-PROD_REGEXP="^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$"
+STAGE_REGEXP="^v(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)-rc(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$"
+PROD_REGEXP="^v(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)$"
 
 if [[ "$CIRCLE_TAG" =~ $PROD_REGEXP ]]; then
+    echo "-------------------------------------------------------------------"
     echo "Production release detected: $CIRCLE_TAG"
+    echo "-------------------------------------------------------------------"
     npm run build:cdn:prod
 
 elif [[ "$CIRCLE_TAG" =~ $STAGE_REGEXP ]]; then
+    echo "-------------------------------------------------------------------"
     echo "Stage release detected: $CIRCLE_TAG"
+    echo "-------------------------------------------------------------------"
     npm run build:cdn:stage
 
 else
+    echo "-------------------------------------------------------------------"
     echo "Dev build for: $CIRCLE_TAG"
+    echo "-------------------------------------------------------------------"
     npm run build:cdn:dev
 fi
--- sync_site.sh
+++ sync_site.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 # check whether the commit is tagged and decide which s3 bucket to push to
-STAGE_REGEXP="^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)-[rc](?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
-PROD_REGEXP="^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$"
+STAGE_REGEXP="^v(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)-rc(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$"
+PROD_REGEXP="^v(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)$"
 
 S3_SUFFIX=dev
 (echo $CIRCLE_TAG | grep -P $STAGE_REGEXP) && S3_SUFFIX=stage
@@ -10,13 +10,21 @@ S3_SUFFIX=dev
 
 aws s3 sync --delete ./build s3://$S3_PREFIX-$S3_SUFFIX/
 
-if [ "${S3_SUFFIX}" = "dev" ]
-then
+if [ "${S3_SUFFIX}" = "dev" ]; then
+  echo "-------------------------------------------------------------------"
+  echo "Push dev release to cloudfrount ..."
+  echo "-------------------------------------------------------------------"
   aws cloudfront create-invalidation --distribution-id $CF_DISTRIBUTION_ID_DEV --paths "/*"
-elif [ "${S3_SUFFIX}" = "stage" ]
-then
+
+elif [ "${S3_SUFFIX}" = "stage" ]; then
+  echo "-------------------------------------------------------------------"
+  echo "Push stage release to cloudfrount ..."
+  echo "-------------------------------------------------------------------"
   aws cloudfront create-invalidation --distribution-id $CF_DISTRIBUTION_ID_STAGE --paths "/*"
-elif [ "${S3_SUFFIX}" = "prod" ]
-then
+
+elif [ "${S3_SUFFIX}" = "prod" ]; then
+  echo "-------------------------------------------------------------------"
+  echo "Push production release to cloudfrount ..."
+  echo "-------------------------------------------------------------------"
   aws cloudfront create-invalidation --distribution-id $CF_DISTRIBUTION_ID_PROD --paths "/*"
 fi
